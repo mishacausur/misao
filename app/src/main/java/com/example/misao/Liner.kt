b7edb8d059867b5776299fc0dd06c7421d93bdde
Misha Causur <mishacausur@mail.ru>
2024-09-02 22:41:09 +0500

    loops

--- app/src/main/java/com/example/misao/Liner.kt
+++ app/src/main/java/com/example/misao/Liner.kt
@@ -7,22 +7,24 @@ class Liner {
         println(name)
     }
 
-    fun check(number: Int?) {
+    fun check(number: Int?) : String {
         val _number = number ?: 0
         val result: String
         when (_number) {
-            1 -> {
-                result = "FIRST"
-            }
-            2 -> {
-                result = "SECOND"
-            }
-            3 -> {
-                result = "THIRD"
-            }
-            else -> {
-                result = "NONE"
-            }
+            1 -> result = "FIRST"
+            2 -> result = "SECOND"
+            3 -> result = "THIRD"
+            else -> result = "NONE"
         }
+
+        return  result
+    }
+
+    fun format() : Int {
+        var result = 0
+        for (i in 1..10) {
+            result += i
+        }
+        return result
     }
 }
